121f16f983001b4bc4c43d30dd6613d37384dccf
karthick-pt6240 <karfthickmtvm@gmail.com>
2022-11-01 11:43:19 +0530

    added scanner

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int ch;
+        Scanner sc = new Scanner(System.in);
         do{
             String username,password;
             username="manger";
