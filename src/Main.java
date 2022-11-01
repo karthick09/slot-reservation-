a4058ee1796749a6e8af3a294ad53756d0cbe923
karthick-pt6240 <karfthickmtvm@gmail.com>
2022-11-01 11:44:56 +0530

    removed scanner

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int ch;
-        Scanner sc = new Scanner(System.in);
         do{
             String username,password;
             username="manger";
